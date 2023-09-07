d0e0c06c652a95d0d9eb265bb4c638469bcaabe9
ksgfan <34235506+ksgfan@users.noreply.github.com>
2023-09-07 10:43:20 +0200

    skip tasks/blocks
    In case of matlab crash, skip tasks and blocks that were already done.

--- master.m
+++ master.m
@@ -21,22 +21,31 @@ dialogID;
 
 %% Run calibration task
 TASK = 'EyeMeasure';
-eyeMovementMeasure_2
-
+if not(isfile([DATA_PATH, filesep, num2str(subject.ID), filesep, num2str(subject.ID), '_Eye.mat']))
+    eyeMovementMeasure_2
+else
+    disp(['EYE MEASURE ALREADY DONE. SKIPPING...'])
+end
 %% Resting state EEG
 TASK = 'Resting';
-
-% Run Resting
-disp('RESTING EEG...');
-Resting_EEG;
-
+if not(isfile([DATA_PATH, filesep, num2str(subject.ID), filesep, num2str(subject.ID), '_Resting.mat']))
+    % Run Resting
+    disp('RESTING EEG...');
+    Resting_EEG;
+else
+    disp(['RESTING ALREADY DONE. SKIPPING...'])
+end
 %% CDA Task
 TASK = 'ManyLabsCDA';
 % Run 5 blocks of 144 trials each
 for BLOCK = 1 : 5
-    % Start the actual task (EEG recording will start here, if TRAINING = 0)
-    disp('CDA TASK...');
-    EEGManyLabs_CDA_18_7_2022; 
+    if not(isfile([DATA_PATH, filesep, num2str(subject.ID), filesep, num2str(subject.ID), '_ManyLabsCDA_block', num2str(BLOCK), '.mat']))
+        % Start the actual task (EEG recording will start here, if TRAINING = 0)
+        disp('CDA TASK...');
+        EEGManyLabs_CDA_18_7_2022; 
+    else
+        disp(['BLOCK ' num2str(BLOCK) ' ALREADY DONE. SKIPPING...'])
+    end
 end
 
 
